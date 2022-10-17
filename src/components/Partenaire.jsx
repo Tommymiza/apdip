a6562d9c1c70fda51a87d2e03b4e70d87e0cb68f
Tommymiza <tommymiza20@gmail.com>
2022-10-17 23:05:22 +0300

    partenaires

--- src/components/Partenaire.jsx
+++ src/components/Partenaire.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import "../style/Partenaire.scss";
+import { partenariat } from "../firebase/partenaire";
+import { motion } from "framer-motion";
 
 const Partenaire = () => {
-  
   return (
-    <div id="partenaire">
-      
-    </div>
+    <motion.div
+      id="partenaire"
+      exit={{ opacity: 0, transition: { duration: 0.5 } }}
+    >
+      <h2>Nos partenaires:</h2>
+      <div className="row-container">
+        {partenariat.map((item) => (
+          <motion.div
+            className="cardpart"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1,transition:{
+              duration: 0.4,
+            }}}
+            
+            key={item.id}
+            onClick={() => {
+              window.open(item.link);
+            }}
+          >
+            <img src={"/images/" + item.img} alt={item.name} />
+            <div className="back"></div>
+            <div className="content">
+              <h3>{item.name}</h3>
+              <p>{item.descri}</p>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </motion.div>
   );
 };
 
---
+++ src/firebase/partenaire.js
@@ -0,0 +1,44 @@
+export const partenariat = [
+    {
+        id: 1,
+        name: "Ambassade de France",
+        descri: "Depuis 19.. - de nos jours",
+        img: "logoAmb-Madagascar.jpg",
+        link: "https://mg.ambafrance.org/"
+    },
+    {
+        id: 2,
+        name:"Afdi-mada",
+        descri: "Afdi est la partenaire officiel d'APDIP depuis sa création",
+        img: "afdi.jpg",
+        link: "https://www.afdi-opa.org/afdi-madagascar/"
+    },
+    {
+        id:3,
+        name: "Réseau SOA",
+        descri: "Formé des Organisations paysannes régionales",
+        img: "logo-SOA-2.png",
+        link: "https://www.fo-mapp.com/farm/reseau-soa-syndicat-des-organisations-agricoles/"
+    },
+    {
+        id: 4,
+        name: "PISCCA",
+        descri: "l’autonomisation économique des femmes et des filles déplacées internes",
+        img: "logo_piscca_ift.jpg",
+        link: "https://bf.ambafrance.org/PISCCA-Appel-a-projets-2022-Autonomisation-economique-des-femmes-et-des-filles"
+    },
+    {
+        id: 5,
+        name: "Leff Armor",
+        descri: "Communaté agricole française",
+        img: "leefarmor.png",
+        link: "https://www.leffarmor.fr/"
+    },
+    {
+        id: 6,
+        name: "Saint Meen Montauban",
+        descri: "Un nouveau point d’étape sur les travaux de réhabilitation de la piscine communautaire Débutés le 3 février 2021 à Saint-Méen-le-Grand",
+        img: "montauban.png",
+        link: "https://www.stmeen-montauban.fr/"
+    }
+]
